bfb18e994d963377e77f54d67774f939cddd0b08
daniel.rojas <daniel.rojas@ospepri.org.ar>
2025-10-01 10:04:38 -0300

    prueba 2

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Configurar zona horaria para Buenos Aires
+// Configurar zona horaria para Buenos Aires aaaaaaaaaa
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 require __DIR__ . '/vendor/autoload.php';
